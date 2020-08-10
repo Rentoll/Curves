c0822faf244d19bd50d8626acb290dd2ea7f639e
Rentol <kukue3636@gmail.com>
2020-08-10 17:21:41 +0300

    Some minor fixes
    Everyone makes mistakes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include "Example.h"
 
-#define __CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
-#define new DEBUG_NEW
-
 int main() {
 	startExample();
 	_CrtDumpMemoryLeaks(); 
